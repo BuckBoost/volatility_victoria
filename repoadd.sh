a541c941fc53d12b6060207e2916e756b0020259
BuckBoost <s_jeelen@hotmail.com>
2016-07-26 10:19:11 -0700

    first commit

---
+++ repoadd.sh
@@ -0,0 +1,8 @@
+echo "deb http://archive.debian.org/debian/ lenny contrib main non-free" > /etc/apt/source.lst
+echo "deb http://archive.debian.org/debian-security lenny/updates main" >> /etc/apt/source.lst
+echo "deb-src http://archive.debian.org/debian/ lenny contrib main non-free" >> /etc/apt/source.lst
+echo "deb-src http://archive.debian.org/debian-security lenny/updates main" >> /etc/apt/source.lst
+
+
+
+
